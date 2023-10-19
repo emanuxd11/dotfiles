ef3b32c6448b17585ed8241fc7bb150384c127b4
Emanuel Maia <tavanomanu@gmail.com>
2023-10-19 01:26:35 +0100

    Fix dmenu power script

--- dmenu/scripts/dmenu_power.sh
+++ dmenu/scripts/dmenu_power.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 # dmenu power menu
-#icons: ⏻ 󰒲 
+# icons: ⏻ 󰒲 
 
 OPT=$(echo -e "⏻ poweroff\n󰒲 suspend\n reboot" | dmenu -c -g 1 -l 3 -fn 'JetBrains Mono-14' -X 12 -Y 934 -W 133)
 
 case $OPT in
-    '⏻ suspend') sudo systemctl suspend   ;;
-    '󰒲 poweroff') sudo systemctl poweroff ;;
-    ' reboot') sudo systemctl reboot     ;;
+    '⏻ poweroff') systemctl poweroff ;;
+    '󰒲 suspend') systemctl poweroff  ;;
+    ' reboot') systemctl reboot     ;;
     *) ;;
 esac
