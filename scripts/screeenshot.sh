30bcf6b9e285dec58ea6bb7d2cec42ea023929d9
Emanuel Maia <tavanomanu@gmail.com>
2023-11-12 23:03:18 +0000

    Add screenshot and pacman database update scripts.

---
+++ scripts/screeenshot.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+DIR="$HOME/Pictures/Screenshots"
+NAME="$DIR/screenshot-$(date +'%Y-%m-%d_%H:%M:%S').png"
+maim "$NAME"
+xclip -selection clipboard -target image/png -i "$NAME"
+notify-send -i "$NAME" "screenshot.sh" "screenshot copied to clipboard"
