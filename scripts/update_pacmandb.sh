78398660c842b86755c16f06a844aa5e60df991c
Emanuel Maia <tavanomanu@gmail.com>
2023-11-13 13:25:03 +0000

    Update pacman script.

--- scripts/update_pacmandb.sh
+++ scripts/update_pacmandb.sh
@@ -2,6 +2,7 @@
 # this is pretty much useless. only point of this script is for the number
 # of updates that shows in my bottom panel to be somewhat accurate by updating
 # day with the help of a cron job that runs this every hour
+# goes in ~/.local/bin
 
 log_date="$(cat /var/log/pacmanscript.log | tail -n 1)"
 log_date=${log_date:1:19}
@@ -10,7 +11,7 @@ current_timestamp=$(date +"%s")
 time_diff=$((current_timestamp - log_timestamp))
 threshold=$((24 * 60 * 60))
 
-if [ "$time_diff" -ge "$threshold" ]; then
+if [ "$time_diff" -ge "$threshold" ] || [ ! -f "/var/log/pacmanscript.log" ]; then
     sudo pacman -Sy -y
     UPD=$(pacman -Qu | wc -l)
     echo "[$(date -u +'%Y-%m-%d %H:%M:%S %Z')] Package database update: $UPD updates available" >> /var/log/pacmanscript.log
